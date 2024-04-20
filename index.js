bbfb30ab34066a4136998d5343daba131fcf4313
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2024-04-20 18:13:50 -0300

    Initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
